9456998f94ecb5b7d6b0f12eeb12e0fef4cacef2
Indra Darmawan <indra.nyoman.id@gmail.com>
2024-10-30 09:36:51 +0700

    add update & delete

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -3,6 +3,11 @@
 <a href="/user/create">Tambah Pengguna Baru</a>
 <ul>
     <?php foreach ($users as $user): ?>
-        <li><?= htmlspecialchars($user['name']) ?> - <?= htmlspecialchars($user['email']) ?></li>
+        <div>
+            <p><?= htmlspecialchars($user['name']) ?> - <?= htmlspecialchars($user['email']) ?>
+            <a href="/user/edit/<?php echo $user['id']; ?>">Edit</a> |
+            <a href="/user/delete/<?php echo $user['id']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
+            </p>
+        </div>
     <?php endforeach; ?>
 </ul>
